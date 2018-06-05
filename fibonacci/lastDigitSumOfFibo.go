a109cc91aea6fe53ad297e881d8b9f1d6e4a5220
ratiblagen <32502100+ratiblagen@users.noreply.github.com>
2018-06-05 17:43:27 +0300

    Create lastDigitSumOfFibo.go

---
+++ fibonacci/lastDigitSumOfFibo.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func Fibo(n int) int {
+	sum := 0
+	array := make([]int, n+1, n+2)
+	if n < 2 {
+		array = array[0:2]
+	}
+	array[0] = 0
+	array[1] = 1
+	for i := 2; i <= n; i++ {
+		array[i] = array[i-1]%10 + array[i-2]%10
+
+		sum = sum + array[i]
+	}
+
+	return sum % 10
+}
+
+func main() {
+
+	fmt.Println(Fibo(8))
+
+}
