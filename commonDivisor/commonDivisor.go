18dc6d4c0890c92b15bec70210a74c9ebb261845
ratiblagen <32502100+ratiblagen@users.noreply.github.com>
2018-06-05 05:32:31 +0300

    Create commonDivisor.go

---
+++ commonDivisor/commonDivisor.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func gcd(x int, y int)int {
+  var current_gcd int
+  current_gcd=1
+for i := 2; i <= x && i <= y; i++ {
+
+if x % i == 0 && y % i == 0 {
+    if i > current_gcd {
+      current_gcd = i;
+    }
+
+}
+
+}
+return current_gcd
+}
+
+func main(){
+fmt.Println(gcd(28851538,1183019))
+
+}
