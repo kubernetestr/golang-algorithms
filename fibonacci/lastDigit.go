d050d55bd327ea070df8bc608f359ca934528857
ratiblagen <32502100+ratiblagen@users.noreply.github.com>
2018-06-05 05:16:04 +0300

    Create lastDigit.go

---
+++ fibonacci/lastDigit.go
@@ -0,0 +1,30 @@
+package main
+
+import  "fmt"
+
+
+func Fibo(n int) int {
+    array := make([]int, n+1, n+2)
+    if n < 2 {
+        array = array[0:2]
+    }
+    array[0] = 0
+    array[1] = 1
+    for i := 2; i <= n; i++ {
+        array[i] = array[i-1]%10 + array[i-2]%10
+    }
+    return array[n]
+}
+
+
+func main() {
+
+
+
+
+
+    fmt.Println(Fibo(327305))
+
+
+
+}
