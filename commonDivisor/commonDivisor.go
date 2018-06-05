d15933816486da60c23cbb666869b129ea2e3c4a
ratiblagen <32502100+ratiblagen@users.noreply.github.com>
2018-06-05 05:37:34 +0300

    Update commonDivisor.go

--- commonDivisor/commonDivisor.go
+++ commonDivisor/commonDivisor.go
@@ -4,7 +4,7 @@ import "fmt"
 func gcd(x int, y int)int {
   var current_gcd int
   current_gcd=1
-for i := 2; i <= x && i <= y; i++ {
+for i := 2; i <= x || i <= y; i++ {
 
 if x % i == 0 && y % i == 0 {
     if i > current_gcd {
