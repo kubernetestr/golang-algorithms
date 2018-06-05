52d9ce6f13aed45e6365d5ae2a2dfb871500b45e
ratiblagen <32502100+ratiblagen@users.noreply.github.com>
2018-06-05 04:40:03 +0300

    Create faster.go

---
+++ fibonacci/faster.go
@@ -0,0 +1,22 @@
+package main
+
+import  "fmt"
+
+
+func Fibo(n int) int {
+    array := make([]int, n+1, n+2)
+    if n < 2 {
+        array = array[0:2]
+    }
+    array[0] = 0
+    array[1] = 1
+    for i := 2; i <= n; i++ {
+        array[i] = array[i-1] + array[i-2]
+    }
+    return array[n]
+}
+
+
+func main() {
+    fmt.Println(Fibo(10))
+}
